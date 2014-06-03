c48d41246e6c51a0f63a8966daa316431d66a66a
rubiscoli <cdliyunlang@gmail.com>
2014-06-02 21:46:27 -0400

    Create plot1.R

---
+++ Exploratory-Data-Analysis/Project-1/plot1.R
@@ -0,0 +1,7 @@
+ #read and cut the file
+ data <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE, as.is = TRUE)
+ data0 <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+ #plot the figure and save as PNG
+ hist(data0[,3], main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+ dev.copy(png, file="plot1.png", height=480, width=480)
+ dev.off()
