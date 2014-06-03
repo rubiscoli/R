c2923d2da947d499a9bcfe92a322afdc076f0f56
rubiscoli <cdliyunlang@gmail.com>
2014-06-02 22:26:06 -0400

    Update plot1.R

--- Exploratory-Data-Analysis/Project-1/plot1.R
+++ Exploratory-Data-Analysis/Project-1/plot1.R
@@ -3,5 +3,5 @@
  data0 <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
  #plot the figure and save as PNG
  hist(data0[,3], main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
- dev.copy(png, file="plot1.png", height=480, width=480)
+ dev.copy(png, file = "plot1.png", height = 480, width = 480)
  dev.off()
