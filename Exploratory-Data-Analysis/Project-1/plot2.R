eed5a6a165c53617253cea033474b795afd21617
rubiscoli <cdliyunlang@gmail.com>
2014-06-02 22:25:42 -0400

    Update plot2.R

--- Exploratory-Data-Analysis/Project-1/plot2.R
+++ Exploratory-Data-Analysis/Project-1/plot2.R
@@ -3,7 +3,7 @@
  data0 <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
  #plot the figure and save as PNG
  plot(strptime(paste(data0[,1],data0[,2]), "%d/%m/%Y %H:%M:%S"), data0[,3], type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
- dev.copy(png, file="plot2.png", height=480, width=480)
+ dev.copy(png, file = "plot2.png", height = 480, width = 480)
  dev.off()
  
  ## The Chinese characters shown in the figure is the same as "Thursday, Friday, and Saturday"!
